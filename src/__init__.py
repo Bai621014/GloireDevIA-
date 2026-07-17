84ddcebe3ec30a6ce747997e119ee5b4077e5b83
BAINIMANE NGANDJEI <bainimanengandjei@gmail.com>
2026-07-17 10:19:03 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,5 @@
-
+# Importations pour rendre les modules accessibles depuis src
+from .web3_manager import verifier_tresorerie
+from .gloire_hub import calculer_valeur_fcfa
+from .gloire_dev_ia import GloireDevIA
+from .security import SecurityAudit
